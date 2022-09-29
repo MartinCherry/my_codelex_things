ef8eb4ea13b97d967c7f2b17a0be3cff0f9c3b9c
Martins Kirsis <109744061+MartinCherry@users.noreply.github.com>
2022-09-29 20:53:54 +0300

    Corrected Encapsulations

---
+++ homeworks-05-2022/src/io/codelex/oop/practice/exercise4/Exercise4.java
@@ -0,0 +1,8 @@
+package io.codelex.oop.practice.exercise4;
+
+public class Exercise4 {
+
+    public static void main(String[] args) {
+        System.out.println(Runner.getRunnersLevel(35));
+    }
+}
